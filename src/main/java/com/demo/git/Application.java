362fb475bc44708c66cd7a2a11f46de29e5cf389
tanushri <tanushri@LAPTOP-L54TKCR9.mshome.net>
2022-04-19 12:24:35 +0530

    commit2

--- src/main/java/com/demo/git/Application.java
+++ src/main/java/com/demo/git/Application.java
@@ -10,7 +10,7 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 		System.out.println("Hello");
 		System.out.println("Demo");
-		System.out.println("1");
+		System.out.println("123");
 		
 	}
 
